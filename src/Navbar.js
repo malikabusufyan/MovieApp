334717202412e1165e8240d55bc55b93f45a5b05
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-06-06 18:29:34 +0530

    Final Submit with optimisation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,22 @@
-import React from "react";
 import styles from "./nav.module.css";
 
-class Navbar extends React.Component {
-  render() {
-    const { cartCount } = this.props;
-    return (
-      <>
-        <div className={styles.nav}>
-          <div className={styles.title}>Movie App</div>
+function Navbar(props) {
+  const { cartCount } = props;
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.title}>Movie App</div>
 
-          <div className={styles.cartIconContainer}>
-            <img
-              className={styles.cartIcon}
-              alt="Cart-Icon"
-              src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
-            />
-            <span className={styles.cartCount}>{cartCount}</span>
-          </div>
+        <div className={styles.cartIconContainer}>
+          <img
+            className={styles.cartIcon}
+            alt="Cart-Icon"
+            src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+          />
+          <span className={styles.cartCount}>{cartCount}</span>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
 export default Navbar;
